3936313e6e43e4d8fa81a2a71626aeb1bd1991a8
marybahati <bahatimary745@gmail.com>
2020-07-28 08:30:52 +0300

    adding register layout

--- app/src/main/java/com/example/hello/RegistrationActivity.kt
+++ app/src/main/java/com/example/hello/RegistrationActivity.kt
@@ -2,12 +2,22 @@ package com.example.hello
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        btnsignup.setOnClickListener{
+            val firstName=etfirstname.text.toString()
+            var lastName=etlastname.text.toString()
+            var phoneNumber=etphonenumber.text.toString()
+            var password=etpassword.text.toString()
+            var passwordConfirmation=etconfirmpassword.text.toString()
+        }
     }
 }
 
