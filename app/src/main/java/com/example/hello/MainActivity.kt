803a78de1d1e4971c4249585d5372b5014fc9a93
Winnie kingori <kingoriwinnie3@gmail.com>
2020-08-07 17:03:54 +0300

    adding courseactivity

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.hello
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_course.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvcourses.layoutManager = LinearLayoutManager(baseContext)
 
-
+        val courseadapter =(listOf <String>
+            ("courseId","courseName","courseCode","Instructer","Description"))
+        rvcourses.adapter = "courseadapter"
+val courseId=(listof(String())
+        val coursename=(listof(String())
+        val coursecode=(listof(String())
+        val instructor=(listof(String())
+        val description=(listof(String())
     }
 
 }
---
+++ app/src/main/java/com/example/hello/courseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hello
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class courseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course)
+    }
+}
